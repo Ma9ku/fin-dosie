b8b36da2aa03da065b34c4f809c21309177b93e6
Grisha <cocarecu@gmail.com>
2023-03-31 14:25:36 +0600

    Material UI added

--- src/components/left-top-frame/LeftTopFrame.jsx
+++ src/components/left-top-frame/LeftTopFrame.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import './leftTopFrame.scss'
 
 function LeftTopFrame(props) {
