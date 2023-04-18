90b0cb8ea217b3d1ed96ea44ff538fa0b2222626
Ma9ku <93972262+Ma9ku@users.noreply.github.com>
2023-04-18 16:06:32 +0600

    dopInfo

--- src/components/dop-info-block/DopInfoBlock.jsx
+++ src/components/dop-info-block/DopInfoBlock.jsx
@@ -1,43 +1,101 @@
 import React, { useState, useEffect } from 'react';
 
+import TextField from '@mui/material/TextField';
+
+import './dopInfoBlock.scss'
+
 function DopInfoBlock(props) {
     return ( 
-        <div className="dopInfoBlock">
+        <div className={"dopInfoBlock " + (props.show ? "showed" : "hidden")}>
             <div>
-                <label htmlFor="">Дата рождения с</label>
-                <input type="text" id='' />
+                <label htmlFor="bdateFrom">Дата рождения с</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='bdateFrom'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor="">Дата рождения по</label>
-                <input type="text" />
+                <label htmlFor="bdayTo">Дата рождения по</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='bdayTo'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor="">Пол</label>
-                <input type="text" />
+                <label htmlFor="gender">Пол</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='gender'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor="">Нация</label>
-                <input type="text" />
+                <label htmlFor="nation">Нация</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='nation'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor="">Страна</label>
-                <input type="text" />
+                <label htmlFor="country">Страна</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='country'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor="">Город</label>
-                <input type="text" />
+                <label htmlFor="city">Город</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='city'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="repCity">Область/город республиканского значения</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='repCity'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="regNum">Регистрационный номер №</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='regNum'
+                variant="outlined" />
             </div>
             <div>
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="transport">VIN/Кузов/Шасси (Транспорт)</label>
+                <TextField sx={{ 
+                    flex: 1, 
+                    borderRadius: "4px",
+                    height: '16px'
+                }}  
+                id='transport'
+                variant="outlined" />
             </div>
         </div>
     );
--- src/components/tab-content-fio/TabContent_FIO.jsx
+++ src/components/tab-content-fio/TabContent_FIO.jsx
@@ -31,14 +31,26 @@ const selectStyle = {
 function TabContent_FIO(props) {
     const [showDopInfo, setShowDopInfo] = useState(false)
 
+    const [fnameType, setFnameType] = useState('begin')
+    const [lnameType, setLnameType] = useState('begin')
+    const [fathNameType, setFathNameType] = useState('begin')
+
     return ( 
         <div className="tab__content">
             <div className="searchForm">
                 <div>
-                    <Select value={"begin"} inputProps={{ 'aria-label': 'Without label' }} style={selectStyle}>
-                        <MenuItem value={"begin"}>Начинается с</MenuItem>
-                        <MenuItem value={"have"}>Содержит</MenuItem>
-                        <MenuItem value={"ends"}>Заканчивается на</MenuItem>
+                    <Select value={fnameType} onChange={event => {
+                        console.log(event)
+                    }} inputProps={{ 'aria-label': 'Without label' }} style={selectStyle}>
+                        <MenuItem value={"begin"} onClick={(event) => {
+                            setFnameType(event.target.dataset.value)
+                        }}>Начинается с</MenuItem>
+                        <MenuItem value={"have"} onClick={(event) => {
+                            setFnameType(event.target.dataset.value)
+                        }}>Содержит</MenuItem>
+                        <MenuItem value={"ends"} onClick={(event) => {
+                            setFnameType(event.target.dataset.value)
+                        }}>Заканчивается на</MenuItem>
                     </Select>
                     <div>
                         <label htmlFor="fname">Имя</label>
@@ -46,10 +58,16 @@ function TabContent_FIO(props) {
                     </div>
                 </div>
                 <div>
-                    <Select value={"begin"} inputProps={{ 'aria-label': 'Without label' }} style={selectStyle}>
-                        <MenuItem value={"begin"}>Начинается с</MenuItem>
-                        <MenuItem value={"have"}>Содержит</MenuItem>
-                        <MenuItem value={"ends"}>Заканчивается на</MenuItem>
+                    <Select value={lnameType} inputProps={{ 'aria-label': 'Without label' }} style={selectStyle}>
+                        <MenuItem value={"begin"} onClick={(event) => {
+                            setLnameType(event.target.dataset.value)
+                        }}>Начинается с</MenuItem>
+                        <MenuItem value={"have"} onClick={(event) => {
+                            setLnameType(event.target.dataset.value)
+                        }}>Содержит</MenuItem>
+                        <MenuItem value={"ends"} onClick={(event) => {
+                            setLnameType(event.target.dataset.value)
+                        }}>Заканчивается на</MenuItem>
                     </Select>
                     <div>
                         <label htmlFor="lName">Фамилия</label>
@@ -57,10 +75,16 @@ function TabContent_FIO(props) {
                     </div>
                 </div>
                 <div>
-                <Select value={"begin"} inputProps={{ 'aria-label': 'Without label' }} style={selectStyle}>
-                    <MenuItem value={"begin"}>Начинается с</MenuItem>
-                    <MenuItem value={"have"}>Содержит</MenuItem>
-                    <MenuItem value={"ends"}>Заканчивается на</MenuItem>
+                <Select value={fathNameType} inputProps={{ 'aria-label': 'Without label' }} style={selectStyle}>
+                    <MenuItem value={"begin"} onClick={(event) => {
+                        setFathNameType(event.target.dataset.value)
+                    }}>Начинается с</MenuItem>
+                    <MenuItem value={"have"} onClick={(event) => {
+                        setFathNameType(event.target.dataset.value)
+                    }}>Содержит</MenuItem>
+                    <MenuItem value={"ends"} onClick={(event) => {
+                        setFathNameType(event.target.dataset.value)
+                    }}>Заканчивается на</MenuItem>
                 </Select>
                     <div>
                         <label htmlFor="fathName">Отчество</label>
@@ -71,10 +95,11 @@ function TabContent_FIO(props) {
 
             <div className='dopInfa'>
 
-                <div onClick={() => setShowDopInfo((prev) => !prev)}>Дополнительные данные</div>
-                {showDopInfo ? 
-                    <DopInfoBlock/>
-                : ""}
+                <div onClick={() => setShowDopInfo((prev) => !prev)}>
+                    <div>Дополнительные данные</div>
+                    <div>{showDopInfo ? "HideIcon" : "ShowIcon"}</div>
+                </div>
+                <DopInfoBlock show={showDopInfo}/>
             </div>
 
             <div style={{display: "flex", justifyContent: "flex-start", paddingLeft: "70%"}}>
